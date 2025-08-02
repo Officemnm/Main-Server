af101fe30dd14ea4e55c11b3776a6bc180cf57ea
Officemnm <ccbdltd.gazipura@gmail.com>
2025-08-02 10:06:36 +0600

    app.py

--- app.py
+++ app.py
@@ -187,8 +187,8 @@ def create_formatted_excel_report(report_data, internal_ref_no=""):
         direct_image_url = 'https://i.ibb.co/v6bp0jQW/rockybilly-regular.webp'
         image_response = requests.get(direct_image_url); image_response.raise_for_status()
         original_img = PILImage.open(BytesIO(image_response.content))
-        padded_img = PILImage.new('RGBA', (original_img.width + 400, original_img.height), (0, 0, 0, 0))
-        padded_img.paste(original_img, (400, 0))
+        padded_img = PILImage.new('RGBA', (original_img.width + 600, original_img.height), (0, 0, 0, 0))
+        padded_img.paste(original_img, (600, 0))
         padded_image_io = BytesIO(); padded_img.save(padded_image_io, format='PNG')
         img = Image(padded_image_io); aspect_ratio = padded_img.height / padded_img.width
         img.width = 60; img.height = int(img.width * aspect_ratio)
@@ -199,7 +199,7 @@ def create_formatted_excel_report(report_data, internal_ref_no=""):
     # --- স্বাক্ষর সেকশন ---
     signature_row = image_row + 1; ws.merge_cells(start_row=signature_row, start_column=1, end_row=signature_row, end_column=NUM_COLUMNS)
     titles = ["Prepared By", "Input Incharge", "Cutting Incharge", "IE & Planning", "Sewing Manager", "Cutting Manager"]
-    signature_cell = ws.cell(row=signature_row, column=1); signature_cell.value = "                ".join(titles); signature_cell.font = bold_font; signature_cell.alignment = Alignment(horizontal='center', vertical='center')
+    signature_cell = ws.cell(row=signature_row, column=1); signature_cell.value = "                     ".join(titles); signature_cell.font = bold_font; signature_cell.alignment = Alignment(horizontal='center', vertical='center')
 
     # --- ফন্ট সাইজ ১৩ করা ---
     last_data_row = current_row - 2
@@ -211,7 +211,7 @@ def create_formatted_excel_report(report_data, internal_ref_no=""):
                 cell.font = new_font
     
     # --- কলামের প্রস্থ ঠিক করা ---
-    ws.column_dimensions['A'].width = 16
+    ws.column_dimensions['A'].width = 18
     
     for i in range(2, NUM_COLUMNS + 1): 
         column_letter = get_column_letter(i)
@@ -340,4 +340,5 @@ def generate_report():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
